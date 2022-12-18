af64dd653eb47eb09f72203766358ffd3d841d39
Stanisław Zagórowski <hasohasacz@gmail.com>
2022-12-18 22:12:03 +0100

    Fix CORS policy

--- weitus-backend/Startup.cs
+++ weitus-backend/Startup.cs
@@ -8,87 +8,89 @@ using weitus_backend.Services;
 
 namespace weitus_backend
 {
-	public class Startup
-	{
-		internal const string corsPolicyName = "CorsPolicy";
+    public class Startup
+    {
+        internal const string corsPolicyName = "CorsPolicy";
 
-		protected IConfiguration Configuration { get; }
+        protected IConfiguration Configuration { get; }
 
-		public Startup(IConfiguration configuration)
-		{
-			Configuration = configuration;
-		}
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
-		public void ConfigureServices(IServiceCollection services)
-		{
-			services.AddCors(options => {
-				options.AddPolicy(corsPolicyName, builder => builder
-					.AllowAnyOrigin()
-					.AllowAnyMethod()
-					.AllowAnyHeader()
-					.AllowCredentials()
-					.Build());
-			});
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy(corsPolicyName, builder => builder
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .Build());
+            });
 
-			services.AddControllers();
-			services.AddEndpointsApiExplorer();
-			services.AddSwaggerGen();
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
 
-			services.AddDbContext<WeitusDbContext>(options =>
-			{
-				options.UseOracle(Configuration.GetConnectionString("OracleConnection"));
-			});
+            services.AddDbContext<WeitusDbContext>(options =>
+            {
+                options.UseOracle(Configuration.GetConnectionString("OracleConnection"));
+            });
 
-			services.AddScoped<IWeitusRepository, WeitusRepository>();
+            services.AddScoped<IWeitusRepository, WeitusRepository>();
 
-			services.AddIdentityCore<WeitusUser>(options =>
-			{
-				options.SignIn.RequireConfirmedAccount = false;
-				options.User.RequireUniqueEmail = true;
-				options.Password.RequireDigit = false;
-				options.Password.RequiredLength = 4;
-				options.Password.RequireNonAlphanumeric = false;
-				options.Password.RequireUppercase = false;
-				options.Password.RequireLowercase = false;
-			})
-			.AddEntityFrameworkStores<WeitusDbContext>();
+            services.AddIdentityCore<WeitusUser>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = false;
+                options.User.RequireUniqueEmail = true;
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 4;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+            })
+            .AddEntityFrameworkStores<WeitusDbContext>();
 
-			services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-				.AddJwtBearer(options =>
-				{
-					options.TokenValidationParameters = new TokenValidationParameters
-					{
-						ValidateIssuer = true,
-						ValidateAudience = true,
-						ValidateLifetime = true,
-						ValidateIssuerSigningKey = true,
-						ValidIssuer = Configuration["Jwt:Issuer"],
-						ValidAudience = Configuration["Jwt:Issuer"],
-						IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
-					};
-				});
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = Configuration["Jwt:Issuer"],
+                        ValidAudience = Configuration["Jwt:Issuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"]))
+                    };
+                });
 
-			services.AddScoped<JwtService>();
-		}
+            services.AddScoped<JwtService>();
+        }
 
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-		{
-			if (env.IsDevelopment())
-			{
-				app.UseDeveloperExceptionPage();
-				app.UseSwagger();
-				app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "weitus_backend v1"));
-			}
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "weitus_backend v1"));
+            }
 
-			app.UseRouting();
+            app.UseRouting();
 
-			app.UseAuthentication();
-			app.UseAuthorization();
+            app.UseCors(corsPolicyName);
 
-			app.UseEndpoints(endpoints =>
-			{
-				endpoints.MapControllers();
-			});
-		}
-	}
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
 }
